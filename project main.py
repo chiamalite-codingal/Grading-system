2b60ff0c70d20247f475cd3bbc2b896f83f8115e
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-09-21 13:25:33 +0100

    project

--- project main.py
+++ project main.py
@@ -5,13 +5,13 @@ s4 = int(input("Enter marks:"))
 s5 = int(input("Enter marks:"))
 total = s1+s2+s3+s4+s5
 avg = total/5
-if avg>=91 and avg<=100:
+if avg>=80 and avg<=90:
     print("A grade")
-elif avg>=81 and avg<=91:
+elif avg>=70 and avg<=80:
     print("B grade")
-elif avg>=71 and avg<=81:
+elif avg>=60 and avg<=70:
     print("C grade")
-elif avg>=61 and avg<=41:
+elif avg>=50 and avg<=30:
     print("D grade")
 else:
     print("E grade-fail")
